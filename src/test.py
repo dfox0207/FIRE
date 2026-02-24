a0eefcecc51b3a66830ff396cfec4f23e200d683
dfox0207 <dfox0207@gmail.com>
2026-02-24 08:09:05 -0500

    remove sys

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-scenario_path = Path(sys.argv[1]) if len(sys.argv) >1 else Path("Config/test.json")
+scenario_path = Path("Config/test.json")
 cfg = json.loads(scenario_path.read_text(encoding="utf-8"))
 
 #2. Convert config values to proper Python types
