02c6798242edcf01be32b253826a38f94db3b0a2
dfox0207 <dfox0207@gmail.com>
2026-02-24 08:13:09 -0500

    order

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ scenario_path = Path("Config/test.json")
 cfg = json.loads(scenario_path.read_text(encoding="utf-8"))
 
 #2. Convert config values to proper Python types
-order: cfg["order"]
+order= cfg["order"]
 
 print(type(order))
 print(order)
