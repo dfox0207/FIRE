fb33777fd90c333b0676f010aecc11d8bb948c3f
dfox0207 <dfox0207@gmail.com>
2026-02-25 10:52:41 -0500

    remaining withdrawal

--- src/test.py
+++ src/test.py
@@ -11,24 +11,22 @@ balances = cfg["balances"]
 print(balances)
 print(type(balances))
 
-# rows = []
+rows = []
 
-# for m in range(8):
-#     withdrawal = 60
-#     row = {"Date": m}
-#     for acct in order:
-#         bal = balances_df.iloc[-1][acct]
-#         if acct in row:
-#             break
-#         elif bal <= withdrawal:
-#             remaining = withdrawal-bal
-#             row[acct] = 0
-#             row[acct+1] = balances_df.iloc[-1][acct+1] - remaining
-#         elif bal == 0:
-#             row[acct] = 0
-#         else:
-#             row[acct] = balances_df.iloc[-1][acct] - withdrawal
-#     rows.append(row)
+for m in range(8):
+    withdrawal = 60
+    remaining_withdrawal = withdrawal
+    row = balances
+    for acct in order:
+        bal = row[acct]
+        if bal <= remaining_withdrawal:
+            remaining_withdrawal = withdrawal-bal
+            row[acct] = 0
+        elif bal == 0:
+            row[acct] = 0
+        else:
+            row[acct] = bal - remaining_withdrawal
+    rows.append(row)
 
-# proj = pd.DataFrame(rows)
-# print(proj) 
+proj = pd.DataFrame(rows)
+print(proj) 
