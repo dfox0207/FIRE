8803a358e61d56f7b74d4bd8eb859bfb2f40653a
dfox0207 <dfox0207@gmail.com>
2026-02-25 10:42:54 -0500

    dictionary

--- src/test.py
+++ src/test.py
@@ -8,26 +8,27 @@ cfg = json.loads(scenario_path.read_text(encoding="utf-8"))
 #2. Convert config values to proper Python types
 order= cfg["order"]
 balances = cfg["balances"]
-balances_df = pd.DataFrame.from_records(balances)
+print(balances)
+print(type(blances))
 
-rows = []
+# rows = []
 
-for m in range(8):
-    withdrawal = 60
-    row = {"Date": m}
-    for acct in order:
-        bal = balances_df.iloc[-1][acct]
-        if acct in row:
-            break
-        elif bal <= withdrawal:
-            remaining = withdrawal-bal
-            row[acct] = 0
-            row[acct+1] = balances_df.iloc[-1][acct+1] - remaining
-        elif bal == 0:
-            row[acct] = 0
-        else:
-            row[acct] = balances_df.iloc[-1][acct] - withdrawal
-    rows.append(row)
+# for m in range(8):
+#     withdrawal = 60
+#     row = {"Date": m}
+#     for acct in order:
+#         bal = balances_df.iloc[-1][acct]
+#         if acct in row:
+#             break
+#         elif bal <= withdrawal:
+#             remaining = withdrawal-bal
+#             row[acct] = 0
+#             row[acct+1] = balances_df.iloc[-1][acct+1] - remaining
+#         elif bal == 0:
+#             row[acct] = 0
+#         else:
+#             row[acct] = balances_df.iloc[-1][acct] - withdrawal
+#     rows.append(row)
 
-proj = pd.DataFrame(rows)
-print(proj) 
+# proj = pd.DataFrame(rows)
+# print(proj) 
