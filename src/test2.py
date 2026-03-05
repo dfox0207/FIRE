95e683fd75d4f17814179a533a78e8f998d299f1
dfox0207 <dfox0207@gmail.com>
2026-03-05 14:55:41 -0500

    plot

--- src/test2.py
+++ src/test2.py
@@ -74,6 +74,66 @@ end_month= ((start_month + pd.DateOffset(years = 30))       #if cashflow end_dat
 
 months = pd.date_range(start_month, end_month, freq="MS")
 
+def plot_networth(df, ax):
+    
+    #read BALANCES.CSV
+    df_bal = pd.read_csv(BALANCES_CSV, parse_dates=["Date"])       
+
+    #identify balance columns (everything except date)
+    balance_cols = [c for c in df_bal.columns if c != "Date"]
+
+    #compute net worth per row
+    df_bal["net_worth"] = df_bal[balance_cols].sum(axis=1)
+
+    #plot Net Worth Actuals
+    df_bal['Date'] = pd.to_datetime(df_bal['Date'])
+    ax.plot(df_bal['Date'],df_bal['net_worth'], label='Actual Balances', linestyle='-', color='b')
+
+    #plot Prjected Net Worth Nominals
+    df['Date'] = pd.to_datetime(df['Date'])
+    ax.plot(df['Date'],df['Net_Worth'], label='Projected Nominal Balances', linestyle='dotted', color='r')
+
+    #plot Prjected Net Worth Nominals
+    df['Date'] = pd.to_datetime(df['Date'])
+    ax.plot(df['Date'],df['Net_Worth_Real'], label='Projected Real Balances', linestyle='dotted', color='g')
+
+    # Format Chart Title and Axises
+    ax.set_title('Net Worth')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Net Worth ($)')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda v, _: f"${v/1e6:.2f}M"))
+    ax.tick_params(axis="x", rotation=45)
+    ax.grid(True)
+    ax.legend()
+
+def plot_accounts(df, ax):              #need to finish adding account balances to plot
+
+    # Format Chart Title and Axises
+    ax.set_title('Account Balances')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Balance ($)')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda v, _: f"${v/1e6:.2f}M"))
+    ax.tick_params(axis="x", rotation=45)
+    ax.grid(True)
+    ax.legend()
+
+def plot_income(df, ax):
+
+    #plot Projected Income
+    df['Date'] = pd.to_datetime(df['Date'])
+    ax.plot(df['Date'],df['Income_Real'], label='Real Income', linestyle='dotted', color='g')
+    
+    # Format Chart Title and Axises
+    ax.set_title('Income- Real')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('($)')
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda v, _: f"${v/1e3:.2f}k"))
+    ax.tick_params(axis="x", rotation=45)
+    ax.grid(True)
+    ax.legend()
 
 #test function
 def cal_withdrawal(m, withdrawal_start_date, withdrawal_type, balances, withdrawal_rate, order):
@@ -179,7 +239,18 @@ def main():
     
     print(json.dumps(cfg, indent=2, sort_keys=True))
 
-    print(projection)
+        # Create two side-by-side subplots
+    fig, (ax1, ax2) = plt.subplots(2, 2, figsize=(14, 5), sharex=True)
+
+    # Left Plot: Networth
+    plot_networth(projection, ax1)
+    plot_accounts(projection, ax1)
+
+    # Right Plot: Income
+    plot_income(projection, ax2)
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
      main()
