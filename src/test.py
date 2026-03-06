352e1fc522974cb4bff9a95e9ce40fd779b448ad
dfox0207 <dfox0207@gmail.com>
2026-03-06 16:40:07 -0500

    list

--- src/test.py
+++ src/test.py
@@ -7,20 +7,20 @@ annual_income = 10000
 income_real = 5000
 
 std_deduct = 15000
-brackets = {
-    0:0.10, 
-    11925:0.12, 
-    48475:0.22,
-    103350:0.24,
-    197300:0.32,
-    250525:0.35,
-    626350:0.37,
-    626351:0.37
-}
+brackets = [
+    (0:0.10), 
+    (11925:0.12), 
+    (48475:0.22),
+    (103350:0.24),
+    (197300:0.32),
+    (250525:0.35),
+    (626350:0.37),
+    (626351:0.37)
+]
 
 
-for b in brackets.keys():
+for i in brackets:
     
-    tax = income_real*brackets[b]
-    print(f"{b}= {tax}")
-    print(f"{b+1}")
+    tax = income_real*brackets[i][1]
+    print(f"{brackets[i][0]}= {tax}")
+    print(f"{brackets[i+1][0]}")
