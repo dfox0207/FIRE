aec66fc432cb01cb9be4d3d6a6e5a143d3ce717e
dfox0207 <dfox0207@gmail.com>
2026-03-10 14:58:42 -0400

    withdraw engine

--- src/projection_engine.py
+++ src/projection_engine.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Tuple
 
 from tax_engine import tax_engine
 from roth_engine import convert_to_roth
-
+from withdraw_engine import calc_withdrawal
 
 def calc_pension(pension_real, retirement, inflation, m):
     pension = 0.0
@@ -23,66 +23,66 @@ def balances_at_date(m, balances, balances_actuals=None):
         return balances_actuals.loc[m, balances.index].astype(float)
     return balances.copy()
 
-def withdrawal_waterfall(balances, withdrawal, order):
-    remaining_withdrawal = withdrawal
-    row = balances.copy()
-    for acct in order:
+# def withdrawal_waterfall(balances, withdrawal, order):
+#     remaining_withdrawal = withdrawal
+#     row = balances.copy()
+#     for acct in order:
         
-        if row[acct] >= remaining_withdrawal:
-            row[acct] -= remaining_withdrawal
-            remaining_withdrawal = 0
-            break
-        else:
-            remaining_withdrawal = remaining_withdrawal-row[acct]
-            row[acct] = 0
+#         if row[acct] >= remaining_withdrawal:
+#             row[acct] -= remaining_withdrawal
+#             remaining_withdrawal = 0
+#             break
+#         else:
+#             remaining_withdrawal = remaining_withdrawal-row[acct]
+#             row[acct] = 0
     
-    balances = row
-    return balances
+#     balances = row
+#     return balances
 
-def cal_withdrawal(
-    *, 
-    m, 
-    withdrawal_start_date, 
-    withdrawal_type, 
-    balances, 
-    withdrawal_rate, 
-    order, 
-    inflation, 
-    annual_w0=None, 
-    t0=None, 
-    balances_actuals=None
-    ):
+# def cal_withdrawal(
+#     *, 
+#     m, 
+#     withdrawal_start_date, 
+#     withdrawal_type, 
+#     balances, 
+#     withdrawal_rate, 
+#     order, 
+#     inflation, 
+#     annual_w0=None, 
+#     t0=None, 
+#     balances_actuals=None
+#     ):
     
-    withdrawal = 0.0
-    if m < withdrawal_start_date:
-        return balances, withdrawal, annual_w0, t0 
+#     withdrawal = 0.0
+#     if m < withdrawal_start_date:
+#         return balances, withdrawal, annual_w0, t0 
 
-    if withdrawal_type== "VPW":
-        withdrawal = float(balances.sum())*float(withdrawal_rate)/12.0
+#     if withdrawal_type== "VPW":
+#         withdrawal = float(balances.sum())*float(withdrawal_rate)/12.0
                 
 
-    elif withdrawal_type == "4pct":
-        if annual_w0 is None:
-            if balances_actuals is not None and withdrawal_start_date in balances_actuals.index:
-                b0 = balances_actuals.loc[withdrawal_start_date, balances.index].astype(float)
-            else:
-                b0 = balances.copy()
+#     elif withdrawal_type == "4pct":
+#         if annual_w0 is None:
+#             if balances_actuals is not None and withdrawal_start_date in balances_actuals.index:
+#                 b0 = balances_actuals.loc[withdrawal_start_date, balances.index].astype(float)
+#             else:
+#                 b0 = balances.copy()
 
-            annual_w0 = withdrawal_rate * float(b0.sum())
-            t0 = withdrawal_start_date
+#             annual_w0 = withdrawal_rate * float(b0.sum())
+#             t0 = withdrawal_start_date
 
-        #Add inflation to withdrawal basis
-        delta_months = (m.to_period("M") - t0.to_period("M")).n
-        annual_withdrawal = annual_w0*(1+inflation)**(delta_months/12)                  #delta_months is negative
-        withdrawal = annual_withdrawal/12.0
+#         #Add inflation to withdrawal basis
+#         delta_months = (m.to_period("M") - t0.to_period("M")).n
+#         annual_withdrawal = annual_w0*(1+inflation)**(delta_months/12)                  #delta_months is negative
+#         withdrawal = annual_withdrawal/12.0
 
-    else:
-        raise ValueError(f"Unknown withdrawal type: {withdrawal_type}")
+#     else:
+#         raise ValueError(f"Unknown withdrawal type: {withdrawal_type}")
 
-    #Take withdrawal from accounts in order
-    balances = withdrawal_waterfall(balances, withdrawal, order)
+#     #Take withdrawal from accounts in order
+#     balances = withdrawal_waterfall(balances, withdrawal, order)
         
-    return balances, withdrawal, annual_w0, t0
+#     return balances, withdrawal, annual_w0, t0
 
 def apply_flows(balances, cf, m):
     active = cf[(cf["start_date"]<=m) & (cf["end_date"].isna() | (cf["end_date"] >= m))]
---
+++ src/withdraw_engine.py
@@ -0,0 +1,61 @@
+
+def withdrawal_waterfall(balances, withdrawal, order):
+    remaining_withdrawal = withdrawal
+    row = balances.copy()
+    for acct in order:
+        
+        if row[acct] >= remaining_withdrawal:
+            row[acct] -= remaining_withdrawal
+            remaining_withdrawal = 0
+            break
+        else:
+            remaining_withdrawal = remaining_withdrawal-row[acct]
+            row[acct] = 0
+    
+    balances = row
+    return balances
+
+def cal_withdrawal(
+    *, 
+    m, 
+    withdrawal_start_date, 
+    withdrawal_type, 
+    balances, 
+    withdrawal_rate, 
+    order, 
+    inflation, 
+    annual_w0=None, 
+    t0=None, 
+    balances_actuals=None
+    ):
+    
+    withdrawal = 0.0
+    if m < withdrawal_start_date:
+        return balances, withdrawal, annual_w0, t0 
+
+    if withdrawal_type== "VPW":
+        withdrawal = float(balances.sum())*float(withdrawal_rate)/12.0
+                
+
+    elif withdrawal_type == "4pct":
+        if annual_w0 is None:
+            if balances_actuals is not None and withdrawal_start_date in balances_actuals.index:
+                b0 = balances_actuals.loc[withdrawal_start_date, balances.index].astype(float)
+            else:
+                b0 = balances.copy()
+
+            annual_w0 = withdrawal_rate * float(b0.sum())
+            t0 = withdrawal_start_date
+
+        #Add inflation to withdrawal basis
+        delta_months = (m.to_period("M") - t0.to_period("M")).n
+        annual_withdrawal = annual_w0*(1+inflation)**(delta_months/12)                  #delta_months is negative
+        withdrawal = annual_withdrawal/12.0
+
+    else:
+        raise ValueError(f"Unknown withdrawal type: {withdrawal_type}")
+
+    #Take withdrawal from accounts in order
+    balances = withdrawal_waterfall(balances, withdrawal, order)
+        
+    return balances, withdrawal, annual_w0, t0
