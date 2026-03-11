1df300c35ab8abd7ad0bc4cf419a07060d981950
dfox0207 <dfox0207@gmail.com>
2026-03-11 12:17:11 -0400

    timestamp

--- src/withdraw_engine.py
+++ src/withdraw_engine.py
@@ -53,22 +53,8 @@ def calc_withdrawal(
     if withdrawal_type== "VPW":
         withdrawal = float(balances.sum())*float(withdrawal_rate)/12.0
                 
-
     elif withdrawal_type == "4pct":
-        withdrawal = classic_withdrawal(m, annual_w0, balances_actuals, withdrawal_start_date, balances, withdrawal_rate, t0, inflation)
-        # if annual_w0 is None:
-        #     if balances_actuals is not None and withdrawal_start_date in balances_actuals.index:
-        #         b0 = balances_actuals.loc[withdrawal_start_date, balances.index].astype(float)
-        #     else:
-        #         b0 = balances.copy()
-
-        #     annual_w0 = withdrawal_rate * float(b0.sum())
-        #     t0 = withdrawal_start_date
-
-        # #Add inflation to withdrawal basis
-        # delta_months = (m.to_period("M") - t0.to_period("M")).n
-        # annual_withdrawal = annual_w0*(1+inflation)**(delta_months/12)                  #delta_months is negative
-        # withdrawal = annual_withdrawal/12.0
+        withdrawal, annual_w0, t0 = classic_withdrawal(m, annual_w0, balances_actuals, withdrawal_start_date, balances, withdrawal_rate, t0, inflation)
 
     else:
         raise ValueError(f"Unknown withdrawal type: {withdrawal_type}")
