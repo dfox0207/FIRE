f27ec5e48e4fe87655f75d516e0bb72831f3dbdd
dfox0207 <dfox0207@gmail.com>
2026-03-20 14:22:33 -0400

    policy

--- src/projection_engine.py
+++ src/projection_engine.py
@@ -162,7 +162,7 @@ def projection_engine(
             order=order, 
             inflation=inflation, 
             rmd_start_age=73,
-            policy = policy,
+            policy = assumptions.get("Optimizer_policy"),
             ytd_tax_buckets = ytd_tax_buckets,
             annual_w0=annual_w0,
             t0=t0,
